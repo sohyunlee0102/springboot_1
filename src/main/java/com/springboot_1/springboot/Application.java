bfc69017208957d54156f40d6ce138d58656b378
shlee <kwlee0100@naver.com>
2024-07-02 02:04:26 +0900

    gitignore 등록

--- src/main/java/com/springboot_1/springboot/Application.java
+++ src/main/java/com/springboot_1/springboot/Application.java
@@ -1,11 +1,14 @@
 package com.springboot_1.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication // 스프링 부트의 자동 설정, 스프링 Bean 읽기와 새성을 모두 자동으로 설정
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 }
